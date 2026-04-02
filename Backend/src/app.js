770c728e09eacee57745fbeeabb2130c3dcea66c
Dheerajverma <dheerajku357@gmail.com>
2026-04-02 21:46:46 +0530

    login and register authentication created in backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,30 @@
-import express from 'express'
+import express from "express"
+import cors from "cors"
+import cookieParser from 'cookie-parser'
 
-const app= express();
+const app=express();
+app.use(cors({
+     origin: process.env.CORS_ORIGIN,
+     credentials:true,
+}))
+          // FOR JSON FILE 
+app.use(express.json({limit:"16mb"}));
+          // FOR URLS  extended use for nested objects 
+app.use(express.urlencoded({extended:true,limit:"16mb"}))
+          //   TO STORE FILE OR MEDIA IN FOLDER
+app.use(express.static("public"))
+          // to read user cookie 
+app.use(cookieParser())
+
+
+
+
+// ROUTES IMPORT ....
+import userRouter from '../src/routes/user.router.js'
+
+
+// routes declaration
+app.use('/user',userRouter);
+
 
 export {app}
---
+++ Backend/src/routes/user.router.js
@@ -0,0 +1,11 @@
+import {Router} from 'express'
+import { loginUser, registerUser } from '../controllers/user.controller.js'
+import { upload } from '../middlewares/multer.middleware.js';
+
+const router = Router();
+
+ router.route('/register').post(upload.none(), registerUser)
+ router.route('/login').post(upload.none(),loginUser)
+
+ export default router
+
